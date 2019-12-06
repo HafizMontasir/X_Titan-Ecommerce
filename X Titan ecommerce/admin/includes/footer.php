10cf54f116e0496477ad59d562906c4076572af2
HafizMontasir <58471393+HafizMontasir@users.noreply.github.com>
2019-12-06 18:51:46 +0800

    my first commit

---
+++ X Titan ecommerce/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Admin: Hafiz Montasir </b>
+    </div>
+    <a href="https://www.facebook.com/mishan.hafiz/">Hafiz Montasir</a></strong>
+</footer>
---
+++ X Titan ecommerce/includes/sidebar.php
@@ -0,0 +1,56 @@
+<div class="row">
+	<div class="box box-solid">
+	  	<div class="box-header with-border">
+	    	<h3 class="box-title"><b>Most Viewed Today</b></h3>
+	  	</div>
+	  	<div class="box-body">
+	  		<ul id="trending">
+	  		<?php
+	  			$now = date('Y-m-d');
+	  			$conn = $pdo->open();
+
+	  			$stmt = $conn->prepare("SELECT * FROM products WHERE date_view=:now ORDER BY counter DESC LIMIT 10");
+	  			$stmt->execute(['now'=>$now]);
+	  			foreach($stmt as $row){
+	  				echo "<li><a href='product.php?product=".$row['slug']."'>".$row['name']."</a></li>";
+	  			}
+
+	  			$pdo->close();
+	  		?>
+	    	<ul>
+	  	</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="box box-solid">
+	  	<div class="box-header with-border">
+	    	<h3 class="box-title"><b>Become a Subscriber</b></h3>
+	  	</div>
+	  	<div class="box-body">
+	    	<p>Get free updates on the latest products and discounts, straight to your inbox.</p>
+	    	<form method="POST" action="">
+		    	<div class="input-group">
+	                <input type="text" class="form-control">
+	                <span class="input-group-btn">
+	                    <button type="button" class="btn btn-info btn-flat"><i class="fa fa-envelope"></i> </button>
+	                </span>
+	            </div>
+		    </form>
+	  	</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class='box box-solid'>
+	  	<div class='box-header with-border'>
+	    	<h3 class='box-title'><b>Follow me on Social Media</b></h3>
+	  	</div>
+	  	<div class='box-body'>
+	    	<a class="btn btn-social-icon btn-facebook" href="https://www.facebook.com/mishan.hafiz"><i class="fa fa-facebook"></i></a>
+	    	<a class="btn btn-social-icon btn-instagram" href="https://www.instagram.com/hafizmontasir/?hl=en"><i class="fa fa-instagram"></i></a>
+	    	<a class="btn btn-social-icon btn-google" href="https://myaccount.google.com/u/1/?utm_source=OGB&utm_medium=app&pli=1"><i class="fa fa-google-plus"></i></a>
+
+	  	</div>
+	</div>
+</div>
